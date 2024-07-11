941f2744d754d6644c10af148d8f00eed5ec505b
pjh8838 <pjh8838@gmail.com>
2024-07-11 14:40:45 +0900

    first commit

---
+++ src/main/java/com/exam/TradersApplication.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradersApplication.class, args);
+	}
+
+}
