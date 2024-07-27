67bb0122212ee2a91a5878cb10b0bbb547d68d8a
yoon9i <todisto29@naver.com>
2024-07-27 22:26:51 +0900

    signup -> image ++

--- src/main/java/com/exam/controller/UserController.java
+++ src/main/java/com/exam/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.exam.controller;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.exam.config.SecurityConfig;
 import com.exam.dto.UserDTO;
@@ -39,24 +46,80 @@ public class UserController {
         this.jwtTokenService = jwtTokenService;
     }
 
-    // 회원가입
+//    // 회원가입
+//    @PostMapping("/signup")
+//    public ResponseEntity<?> signup(@RequestBody UserDTO userDTO) {
+//        try {
+//            log.debug("Received signup request: {}", userDTO);
+//
+//            // 비밀번호 암호화
+//            String encryptedPassword = new BCryptPasswordEncoder().encode(userDTO.getPasswd());
+//            userDTO.setPasswd(encryptedPassword);
+//
+//            userService.save(userDTO);
+//
+//            return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
+//        } catch (Exception e) {
+//            log.error("Signup error: ", e);
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Signup failed: " + e.getMessage());
+//        }
+//    }
+    
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> signup(
+            @RequestParam("branchId") String branchId,
+            @RequestParam("passwd") String passwd,
+            @RequestParam("branchName") String branchName,
+            @RequestParam("nickname") String nickname,
+            @RequestParam("branchNum") String branchNum,
+            @RequestParam("post") String post,
+            @RequestParam("addr1") String addr1,
+            @RequestParam("addr2") String addr2,
+            @RequestParam("phoneNum") String phoneNum,
+            @RequestParam("branchImage") MultipartFile branchImage) {
+
+        String uploadDir = "C:\\springboot_study\\sts-4.22.1.RELEASE\\workspace\\Traders\\src\\main\\resources\\static\\images";
         try {
-            log.debug("Received signup request: {}", userDTO);
-
             // 비밀번호 암호화
-            String encryptedPassword = new BCryptPasswordEncoder().encode(userDTO.getPasswd());
-            userDTO.setPasswd(encryptedPassword);
+            String encryptedPassword = new BCryptPasswordEncoder().encode(passwd);
 
+            // 파일 저장
+            String fileName = branchImage.getOriginalFilename();
+            Path path = Paths.get(uploadDir).resolve(fileName);
+            Files.createDirectories(path.getParent());
+            try (InputStream inputStream = branchImage.getInputStream()) {
+                Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            // UserDTO 객체 생성 및 설정
+            UserDTO userDTO = new UserDTO();
+            userDTO.setBranchId(branchId);
+            userDTO.setPasswd(encryptedPassword);
+            userDTO.setBranchName(branchName);
+            userDTO.setNickname(nickname);
+            userDTO.setBranchNum(branchNum);
+            userDTO.setPost(post);
+            userDTO.setAddr1(addr1);
+            userDTO.setAddr2(addr2);
+            userDTO.setPhoneNum(phoneNum);
+            userDTO.setBranchImage(fileName); // 파일 이름만 저장 (경로가 아닌)
+
+            // 데이터베이스에 저장
             userService.save(userDTO);
 
-            return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+        } catch (DataIntegrityViolationException e) {
+            log.error("데이터 무결성 오류: ", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("데이터 무결성 오류: " + e.getMessage());
         } catch (Exception e) {
-            log.error("Signup error: ", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Signup failed: " + e.getMessage());
+            log.error("회원가입 오류: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("회원가입 실패: " + e.getMessage());
         }
     }
+
+
+
+
     
     // 로그인 처리 + token 생성후 반환
     @PostMapping("/login")
@@ -110,4 +173,3 @@ public class UserController {
 //        return userService.findAll();
 //    }
 }
-
