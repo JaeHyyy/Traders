0a11cc97a4117fa3fdbc11d1f9d8d98ff2c2bab4
dldofls666 <dldofls666@gmail.com>
2024-08-02 09:16:23 +0900

    시큐리티 /ordercart/** 경로 추가

--- src/main/java/com/exam/security/JwtSecurityFilterChainConfig.java
+++ src/main/java/com/exam/security/JwtSecurityFilterChainConfig.java
@@ -43,7 +43,7 @@ public class JwtSecurityFilterChainConfig {
 
 	                auth.antMatchers("/","/signup","/login","/success","/users","/hello","/home", 
 	                		"/home/{keyword}","/receipt","/api/**","/movdateasc","/movdatedesc",
-	                		"/stock","/ordercart","/home/save","/images/items/{filename}","/images/**",
+	                		"/stock","/ordercart/**","/home/save","/images/items/{filename}","/images/**",
 	                		"/disuse/**","/ordercart/saveAll","api/qrcode?date=${movdate}","/receiptmodify/**",
 	                		"/updateLocation/**","/getLocation/**","/findgcode/**","/join/**").permitAll()  // 회원가입 요청 허용.
 
